b264b4a5f5be0b3bbd58a1f43e6add0d8e1c12c1
Dewha <scherbinin96@gmail.com>
2020-12-02 16:55:45 +0500

    Создал проект
    Создал проект, добавил загрузочный экран и форму  авторизации, добавил анимации

---
+++ EsWallet/app/src/main/java/com/example/eswallet/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.eswallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //var
+    private static int SPLACH_SCREEN = 3000;
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+
+        //init
+        image = findViewById(R.id.iv_logo);
+        logo = findViewById(R.id.tv_logo);
+
+        //animation
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
+        image.startAnimation(topAnim);
+        logo.startAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(image,"logo_image");
+                pairs[1] = new Pair<View,String>(logo,"logo_text");
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options;
+                    options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                    startActivity(intent, options.toBundle());
+                }
+            }
+        },SPLACH_SCREEN);
+    }
+}
